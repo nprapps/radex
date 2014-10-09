085c52cb3a50ce2226f2a11891d26ccc1df3e4b2
lindamood <wesley.lindamood@gmail.com>
2014-10-09 09:22:29 -0400

    removing big video code and adding html5/css solution instead

--- www/js/app.js
+++ www/js/app.js
@@ -2,17 +2,11 @@ $(document).ready(function() {
 
     var sound = new Howl({
       urls: ['assets/bayaka.mp3'],
-      autoplay: true,
+      autoplay: false,
       loop: true,
       volume: 0.7,
     });
-    
-    
-    var BV = new $.BigVideo();
-    BV.init();
-    BV.show('assets/fire.mp4',{ambient:true,doLoop:true});
-    
-    
+
     
     $('#ex1-play').on('click', function(){
 		sound.stop().play();
@@ -20,6 +14,9 @@ $(document).ready(function() {
 	$('#ex1-pause').on('click', function(){
 		sound.pause();
 	});
+	
+	//mute all videos
+	$("video").prop('muted', true);
     
     
 });
